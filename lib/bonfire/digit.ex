90726e5097e360987cc9583e50a1e8b6abedec4b
Declan Whelan <dwhelan@dpwhelan.com>
2019-09-12 01:53:02 -0400

    Delegate decode to Decode.apply

--- lib/bonfire/digit.ex
+++ lib/bonfire/digit.ex
@@ -1,3 +1,44 @@
+defmodule Rule do
+  defmacro defrule do
+    quote do
+      def decode([_ | _] = list) do
+        decode({list, ''})
+      end
+    end
+  end
+
+  defmacro defrule(do: block) do
+    quote do
+      def decode([_ | _] = list) do
+        decode({list, ''})
+      end
+
+      unquote(block)
+
+      def decode(_) do
+        nil
+      end
+
+      def encode([char | rest]) when is_digit(char) do
+        {[char], rest}
+      end
+
+    end
+  end
+
+  defmacro __using__(opts \\ []) do
+    quote do
+      import Rule, unquote(opts)
+    end
+  end
+
+  defmacro defcodec(module) do
+    quote do
+
+    end
+  end
+end
+
 defmodule Digit do
   @moduledoc """
 
@@ -28,6 +69,7 @@ defmodule Digit do
       nil
   """
   import Guards
+  use Rule
 
   defmodule Decode do
     def apply({[char | chars], rest}) when is_digit(char) do
@@ -41,32 +83,17 @@ defmodule Digit do
     end
   end
 
-  # could be in a defelement macro
-  def decode([_ | _] = list) do
-    decode({list, ''})
+  defrule do
+#    defdelegate decode(left, right), to: Decode, as: :apply
+    def decode({[char | rest], foo}) when is_digit(char) do
+      Decode.apply({[char], rest ++ foo})
+    end
+
+    def encode({[char | rest], foo}) when is_digit(char) do
+      Encode.apply({[char], rest ++ foo})
+    end
   end
 
-  def decode({[char | rest], foo}) when is_digit(char) do
-    {[char], rest ++ foo}
-  end
-
-
-  def decode({[char | rest], foo}) when is_digit(char) do
-    Decode.apply {[char], rest ++ foo}
-  end
-
-  def decode(_) do
-    nil
-  end
-
-  # could be in a defelement macro
-  def encode([char | rest]) when is_digit(char) do
-    {[char], rest}
-  end
-
-  def encode({[char | rest], foo}) when is_digit(char) do
-    Encode.apply {[char], rest ++ foo}
-  end
 
   def encode(_) do
     nil
