8eca91ffb47c9ded8395f053f6e9abf3842b326b
Declan Whelan <dwhelan@dpwhelan.com>
2019-09-13 17:44:47 -0400

    add types specs to Decode and Encode modules

--- lib/bonfire/codec.ex
+++ lib/bonfire/codec.ex
@@ -12,12 +12,12 @@ defmodule Codec do
     codec = __CALLER__.module
 
     quote do
-      @spec decode(nonempty_list(char)) :: {nonempty_list(char), list(char)}
+      @spec decode(nonempty_list(char)) :: {nonempty_list(char), list(char)} | nil
       def decode([_ | _] = source) do
         decode({[], source})
       end
 
-      @spec decode({list(char), nonempty_list(char)}) :: {nonempty_list(char), list(char)}
+      @spec decode({list(char), nonempty_list(char)}) :: {nonempty_list(char), list(char)} | nil
       def decode({dest, [_ | _] = source}) do
         case unquote(codec).Decode.apply({Enum.reverse(dest), source}) do
           nil -> nil
@@ -26,6 +26,7 @@ defmodule Codec do
       end
 
       defmodule Decode do
+        @spec apply({list(char), nonempty_list(char)}) :: {nonempty_list(char), list(char)} | nil
         def apply({dest, [char | rest]}) when is_list(dest) do
           case unquote(predicate).(char) do
             true -> {[char | dest], rest}
@@ -34,12 +35,12 @@ defmodule Codec do
         end
       end
 
-      @spec encode(nonempty_list(char)) :: {list(char), nonempty_list(char)}
+      @spec encode(nonempty_list(char)) :: {list(char), nonempty_list(char)} | nil
       def encode([_ | _] = source) do
         encode({source, []})
       end
 
-      @spec encode({nonempty_list(char), list(char)}) :: {list(char), nonempty_list(char)}
+      @spec encode({nonempty_list(char), list(char)}) :: {list(char), nonempty_list(char)} | nil
       def encode({[_ | _] = source, dest}) do
         case unquote(codec).Encode.apply({source, Enum.reverse(dest)}) do
           nil -> nil
@@ -48,6 +49,7 @@ defmodule Codec do
       end
 
       defmodule Encode do
+        @spec apply({nonempty_list(char), list(char)}) :: {list(char), nonempty_list(char)} | nil
         def apply({[char | rest], dest} = input) do
           case unquote(predicate).(char) do
             true -> {rest, [char | dest]}
