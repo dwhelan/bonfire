4ff0dcf378170be2253f6e3b96cf6fde4c941e59
Declan Whelan <dwhelan@dpwhelan.com>
2019-09-20 15:02:10 -0400

    rename A to Alpha

--- test/bonfire/lists_right_test.exs
+++ test/bonfire/lists_right_test.exs
@@ -1,23 +1,15 @@
-defmodule A do
-  defmodule Right do
-    # will move an `?a` to the right, nil otherwise
-    @behaviour :"Elixir.Right"
+defmodule Alpha.Right do
+  # will move an alpha character to the right, nil otherwise
+  @behaviour :"Elixir.Right"
 
-    def apply({[?a | _] = _, _} = input) do
-      Lists.Right.move_one(input)
-    end
+  alias Lists.Right
 
-    def apply(_) do
-      nil
-    end
+  def move_one({[char | _], _} = input) when char in ?a..?z or char in ?A..?Z do
+    Right.move_one(input)
+  end
 
-    def move_one({[?a | _] = _, _} = input) do
-      Lists.Right.move_one(input)
-    end
-
-    def move_one(_) do
-      nil
-    end
+  def move_one(_) do
+    nil
   end
 end
 
@@ -40,9 +32,9 @@ defmodule Lists.RightTest do
   end
 
   test "move_one/2 with a Lists" do
-    assert move_one({'a', '_'}, A) == {'', 'a_'}
-    assert move_one({'b', '_'}, A) == nil
-    assert move_one({'', '_'}, A) == nil
+    assert move_one({'a', '_'}, Alpha) == {'', 'a_'}
+    assert move_one({'*', '_'}, Alpha) == nil
+    assert move_one({'', '_'}, Alpha) == nil
   end
 
   test "wrap/1" do
